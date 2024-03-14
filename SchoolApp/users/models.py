87bf14065d0e7b225c0c81d4c0b7eca35c724485
Vnzmiau <viliyan.balchev@gmail.com>
2024-03-14 18:58:38 +0200

    Create custom User
    User

--- SchoolApp/users/models.py
+++ SchoolApp/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    last_name=models.CharField(max_length=100, null=True)
+    email=models.EmailField(unique=True)
+    #avatar
 
 # Create your models here.
