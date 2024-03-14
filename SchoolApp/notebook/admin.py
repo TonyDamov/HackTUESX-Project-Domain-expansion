c5523065e276cd9626c473aaa19569db14beabaa
TonyDamov <antoan.damov@gmail.com>
2024-03-14 10:36:10 +0200

    Added Grades class in DB and display info in admin page

--- SchoolApp/notebook/admin.py
+++ SchoolApp/notebook/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Grade
 # Register your models here.
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin) :
+    list_display = ('grade', 'user')
--- SchoolApp/notebook/models.py
+++ SchoolApp/notebook/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Grade(models.Model) :
+    grade = models.DecimalField(null=False, blank=False, decimal_places = 2, max_digits = 3)
+    user = models.ForeignKey(User, models.CASCADE)
+    def __str__(self) -> str:
+        return str(self.grade)
