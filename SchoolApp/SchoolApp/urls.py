583627690cbb5007b7cd0952f76c9cf674dfb8c1
Lppetrov0 <demirando8@abv.bg>
2024-03-14 16:13:40 +0200

    Working Register, Login and Home page

--- SchoolApp/SchoolApp/urls.py
+++ SchoolApp/SchoolApp/urls.py
@@ -18,10 +18,17 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from notebook import views as note_views
+from users import views as user_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("notebook.urls")),
+    path('materials/',note_views.Material,name = 'material-page'),
+    path('grades/',note_views.Grades,name = 'your-grades'),
+    path('home/',note_views.home,name = 'home-page'),
+    path('',user_views.loginPage,name = 'login-page'),
+    path('register/',user_views.registerPage,name = 'register-page'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
