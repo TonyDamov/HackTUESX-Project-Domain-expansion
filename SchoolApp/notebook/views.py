0fa74827465db7cec5435722186a3e029e6d0a70
TonyDamov <antoan.damov@gmail.com>
2024-03-14 19:52:34 +0200

    ViewClasses

--- SchoolApp/notebook/views.py
+++ SchoolApp/notebook/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Grade,Material
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -13,6 +14,15 @@ def Materials(request):
     materials = Material.objects.all()
     return render(request,'notebook/materials.html',{'materials':materials})
 
+class MaterialsListView(ListView):
+    model = Material
+    template_name = 'notebook/materials.html'
+    context_object_name = 'materials'
+    ordering = ['-date_posted']
+
+class MaterialsDetailView(DetailView):
+    model = Material
+    context_object_name = 'materials'
 
 def Grades(request):
     grades = Grade.objects.all()
