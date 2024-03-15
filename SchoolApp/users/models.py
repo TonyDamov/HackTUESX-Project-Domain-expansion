6385f572f8d140c7537c41f0a77950668d9c3b0f
TonyDamov <antoan.damov@gmail.com>
2024-03-15 09:33:46 +0200

    Fixed file downloads

--- SchoolApp/users/models.py
+++ SchoolApp/users/models.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     last_name=models.CharField(max_length=100, null=True)
     email=models.EmailField(unique=True)
-    avatar=models.ImageField(default='default.jpg', upload_to='profile_pictures')
+    avatar=models.ImageField(default='default.jpg')
 
+    @property
+    def image_url(self):
+        if self.avatar and hasattr(self.avatar, 'url'):
+            return self.avatar.url
+        else:
+            return ""
 # Create your models here.
