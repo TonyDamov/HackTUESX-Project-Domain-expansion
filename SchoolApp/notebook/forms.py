e735a4cd2bfa2fc2f02e3b9c47d9c782010fa72b
Vnzmiau <viliyan.balchev@gmail.com>
2024-03-16 03:09:17 +0200

    Aded form for grade

--- SchoolApp/notebook/forms.py
+++ SchoolApp/notebook/forms.py
@@ -1,9 +1,27 @@
 from django.forms import ModelForm
-from .models import Material
+from .models import Material,Grade
+from users.models import User
 
 class MaterialForm(ModelForm):
     class Meta:
         model=Material
         fields = ['title','description','file','groups']
+
+class GradeForm(ModelForm):
+    class Meta:
+        model=Grade
+        fields='__all__'
+        exclude=['teacher']
         
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user'].queryset = User.objects.filter(role='Student')
     
+class EditGradeForm(ModelForm):
+    class Meta:
+        model=Grade
+        fields=['grade']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user'].queryset = User.objects.filter(role='Student')
